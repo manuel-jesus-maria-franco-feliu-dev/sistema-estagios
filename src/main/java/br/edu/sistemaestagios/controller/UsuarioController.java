69a65819af36296a2f312bf580a4b664ec3485be
Manuel Jesus Maria Franco Feliu <mfrancof_enginf@yahoo.com>
2026-05-01 13:58:09 -0300

    Implementando listagem de usuarios no CRUD

--- src/main/java/br/edu/sistemaestagios/controller/UsuarioController.java
+++ src/main/java/br/edu/sistemaestagios/controller/UsuarioController.java
@@ -1,9 +1,10 @@
-package br.edu.sistemaestagios.controller;
-import br.edu.sistemaestagios.model.Perfil;
+package br.edu.sistemaestagios.sistemaestagios.controller;
+
 import br.edu.sistemaestagios.model.Usuario;
 import br.edu.sistemaestagios.repository.UsuarioRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -12,10 +13,16 @@ public class UsuarioController {
     public UsuarioController(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
+    //CRIAR ARQUIVO DE USUARIOS
+
     @PostMapping
     public Usuario criarUsuario(@RequestBody Usuario usuario) {
         usuario.setAtivo(true);
         return usuarioRepository.save(usuario);
     }
-
+    //LISTAR TODOS OS USUARIOS
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
 }
